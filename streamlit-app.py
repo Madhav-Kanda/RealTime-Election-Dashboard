55d9ae42d4a0d774c6c3869199c014accb077271
Madhav Kanda <madhav.kanda@iitgn.ac.in>
2024-08-01 23:59:10 +0530

    streamlit-app chart insertion

--- streamlit-app.py
+++ streamlit-app.py
@@ -4,6 +4,9 @@ import psycopg2
 from kafka import KafkaConsumer
 import simplejson as json
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from streamlit_autorefresh import st_autorefresh
 
 @st.cache_data
 def fetch_voting_stats():
@@ -45,6 +48,62 @@ def fetch_data_from_kafka(consumer):
 
     return data
 
+def plot_colored_bar_chart(results):
+    data_type = results['candidate_name']
+    colors = plt.cm.viridis(np.linspace(0,1,len(data_type)))
+    plt.bar(data_type, results['total_votes'], color=colors)
+    plt.xlabel('Candidate')
+    plt.ylabel("Total Votes")
+    plt.xticks(rotation=90)
+    return plt
+
+def plot_donut_chart(data):
+    labels = list(data['candidate_name'])
+    sizes = list(data['total_votes'])
+
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
+    ax.axis('equal')
+    plt.title("Candidates Votes")
+    return fig
+
+@st.cache_data(show_spinner=False)
+def split_frame(input_df, rows):
+    df = [input_df.loc[i: i + rows - 1, :] for i in range(0, len(input_df), rows)]
+    return df
+
+def paginate_table(table_data):
+    top_menu = st.columns(3)
+    with top_menu[0]:
+        sort = st.radio("Sort Data", options=["Yes", "No"], horizontal=1, index=1)
+    if sort == "Yes":
+        with top_menu[1]:
+            sort_field = st.selectbox("Sort By", options=table_data.columns)
+        with top_menu[2]:
+            sort_direction = st.radio(
+                "Direction", options=["⬆️", "⬇️"], horizontal=True
+            )
+        table_data = table_data.sort_values(
+            by=sort_field, ascending=sort_direction == "⬆️", ignore_index=True
+        )
+    pagination = st.container()
+
+    bottom_menu = st.columns((4, 1, 1))
+    with bottom_menu[2]:
+        batch_size = st.selectbox("Page Size", options=[10, 25, 50, 100])
+    with bottom_menu[1]:
+        total_pages = (
+            int(len(table_data) / batch_size) if int(len(table_data) / batch_size) > 0 else 1
+        )
+        current_page = st.number_input(
+            "Page", min_value=1, max_value=total_pages, step=1
+        )
+    with bottom_menu[0]:
+        st.markdown(f"Page **{current_page}** of **{total_pages}** ")
+
+    pages = split_frame(table_data, batch_size)
+    pagination.dataframe(data=pages[current_page - 1], use_container_width=True)
+
 def update_data():
     last_refresh = st.empty()
     last_refresh.text(f"Last refresh at: {time.strftime('%Y-%m-%d %H:%M:%S')}")
@@ -76,9 +135,53 @@ def update_data():
         st.subheader(leading_candidate['party_affiliation'])
         st.subheader('Total Vote:{}'.format(leading_candidate['total_votes']))
 
+    # Display the statistics and visualization
+    st.markdown("""----""")
+    st.header('Voting Statistics')
+    results = results[['candidate_id', 'candidate_name', 'party_affiliation', 'total_votes']]
+    results = results.reset_index(drop = True)
+
+    # Display the bar charts
+    col1, col2 = st.columns(2)
+
+    with col1:
+        bar_fig = plot_colored_bar_chart(results)
+        st.pyplot(bar_fig)
+
+    with col2:
+        donut_fig = plot_donut_chart(results)
+        st.pyplot(donut_fig)
+
+    st.table(results)
+
+    # Fetch data from kafka an aggregated turnout by location
+    location_consumer = create_kafka_consumer('aggregated_turnout_by_location')
+    location_data = fetch_data_from_kafka(location_consumer)
+    location_result = pd.DataFrame(location_data)
+
+    # max location indentification
+    location_result = location_result.loc[location_result.groupby('state')['count'].idxmax()]
+    location_result = location_result.reset_index(drop=True)
+
+    # display the location of voters
+    st.header('Location of Voters')
+    paginate_table(location_result) 
+
+    st.session_state['last_update']  = time.time()
 
 st.title("Realtime Voting Dashboard")
 topic_name = "aggregated_votes_per_candidates"
 
+def sidebar():
+    if st.session_state.get('latest_update') is None:
+        st.session_state['last_update'] = time.time()
 
+    refresh_interval = st.sidebar.slider("Refresh interval (seconds)", 5, 60, 10)
+    st_autorefresh(interval=refresh_interval * 1000, key="auto")
+
+    # Button to manually refresh data
+    if st.sidebar.button('Refresh Data'):
+        update_data()
+
+sidebar()
 update_data()
